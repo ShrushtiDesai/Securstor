c16293cc70d9622bb267914a576df6d976953e23
ShrushtiDesai <shrushtidesai02@gmail.com>
2024-08-22 20:46:36 +0530

    Update Layout.jsx

--- Client/src/Layout.jsx
+++ Client/src/Layout.jsx
@@ -28,7 +28,7 @@ function Layout() {
         // console.log(account)
         setAccount(address);
 
-        const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+        const contractAddress = "your contract address here";
 
         const contract = new ethers.Contract(
           contractAddress,
